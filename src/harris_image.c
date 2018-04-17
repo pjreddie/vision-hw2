31acaf7d86eb28cfcd2a0808dba3e9f5fa3c1e22
Joseph Redmon <pjreddie@gmail.com>
2018-04-16 20:30:58 -0700

    skeleton code

---
+++ src/harris_image.c
@@ -0,0 +1,180 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include "image.h"
+#include "matrix.h"
+#include <time.h>
+
+// Frees an array of descriptors.
+// descriptor *d: the array.
+// int n: number of elements in array.
+void free_descriptors(descriptor *d, int n)
+{
+    int i;
+    for(i = 0; i < n; ++i){
+        free(d[i].data);
+    }
+    free(d);
+}
+
+// Create a feature descriptor for an index in an image.
+// image im: source image.
+// int i: index in image for the pixel we want to describe.
+// returns: descriptor for that index.
+descriptor describe_index(image im, int i)
+{
+    int w = 5;
+    descriptor d;
+    d.p.x = i%im.w;
+    d.p.y = i/im.w;
+    d.data = calloc(w*w*im.c, sizeof(float));
+    d.n = w*w*im.c;
+    int c, dx, dy;
+    int count = 0;
+    // If you want you can experiment with other descriptors
+    // This subtracts the central value from neighbors
+    // to compensate some for exposure/lighting changes.
+    for(c = 0; c < im.c; ++c){
+        float cval = im.data[c*im.w*im.h + i];
+        for(dx = -w/2; dx < (w+1)/2; ++dx){
+            for(dy = -w/2; dy < (w+1)/2; ++dy){
+                float val = get_pixel(im, i%im.w+dx, i/im.w+dy, c);
+                d.data[count++] = cval - val;
+            }
+        }
+    }
+    return d;
+}
+
+// Marks the spot of a point in an image.
+// image im: image to mark.
+// ponit p: spot to mark in the image.
+void mark_spot(image im, point p)
+{
+    int x = p.x;
+    int y = p.y;
+    int i;
+    for(i = -9; i < 10; ++i){
+        set_pixel(im, x+i, y, 0, 1);
+        set_pixel(im, x, y+i, 0, 1);
+        set_pixel(im, x+i, y, 1, 0);
+        set_pixel(im, x, y+i, 1, 0);
+        set_pixel(im, x+i, y, 2, 1);
+        set_pixel(im, x, y+i, 2, 1);
+    }
+}
+
+// Marks corners denoted by an array of descriptors.
+// image im: image to mark.
+// descriptor *d: corners in the image.
+// int n: number of descriptors to mark.
+void mark_corners(image im, descriptor *d, int n)
+{
+    int i;
+    for(i = 0; i < n; ++i){
+        mark_spot(im, d[i].p);
+    }
+}
+
+// Creates a 1d Gaussian filter.
+// float sigma: standard deviation of Gaussian.
+// returns: single row image of the filter.
+image make_1d_gaussian(float sigma)
+{
+    // TODO: optional, make separable 1d Gaussian.
+    return make_image(1,1,1);
+}
+
+// Smooths an image using separable Gaussian filter.
+// image im: image to smooth.
+// float sigma: std dev. for Gaussian.
+// returns: smoothed image.
+image smooth_image(image im, float sigma)
+{
+    if(1){
+        image g = make_gaussian_filter(sigma);
+        image s = convolve_image(im, g, 1);
+        free_image(g);
+        return s;
+    } else {
+        // TODO: optional, use two convolutions with 1d gaussian filter.
+        // If you implement, disable the above if check.
+        return s2;
+    }
+}
+
+// Calculate the structure matrix of an image.
+// image im: the input image.
+// float sigma: std dev. to use for weighted sum.
+// returns: structure matrix. 1st channel is Ix^2, 2nd channel is Iy^2,
+//          third channel is IxIy.
+image structure_matrix(image im, float sigma)
+{
+    image S = make_image(im.w, im.h, 3);
+    // TODO: calculate structure matrix for im.
+    return S;
+}
+
+// Perform non-max supression on an image of feature responses.
+// image im: 1-channel image of feature responses.
+// int w: distance to look for larger responses.
+// returns: image with only local-maxima responses within w pixels.
+image nms_image(image im, int w)
+{
+    image r = copy_image(im);
+    // TODO: perform NMS on the response map.
+    // for every pixel in the image:
+    //     for neighbors within w:
+    //         if neighbor response greater than pixel response:
+    //             set response to be very low (I use -999999 [why not 0??])
+    return r;
+}
+
+// Find and draw corners on an image.
+// image im: input image.
+// float sigma: std. dev for harris.
+// float thresh: threshold for cornerness.
+// int nms: distance to look for local-maxes in response map.
+void detect_and_draw_corners(image im, float sigma, float thresh, int nms)
+{
+    int n = 0;
+    descriptor *d = harris_corner_detector(im, sigma, thresh, nms, &n);
+    mark_corners(im, d, n);
+}
+
+// Perform harris corner detection and extract features from the corners.
+// image im: input image.
+// float sigma: std. dev for harris.
+// float thresh: threshold for cornerness.
+// int nms: distance to look for local-maxes in response map.
+// int *n: pointer to number of corners detected, should fill in.
+// returns: array of descriptors of the corners in the image.
+descriptor *harris_corner_detector(image im, float sigma, float thresh, int nms, int *n)
+{
+    int i;
+    image S = structure_matrix(im, sigma);
+    image R = make_image(im.w, im.h, 1);
+    // TODO: fill in R, "cornerness" for each pixel using the structure matrix.
+    // We'll use formulation det(S) - alpha * trace(S), alpha = .06.
+
+    // Run NMS on the responses
+    image Rnms = nms_image(R, nms);
+
+
+    //TODO: count number of responses over threshold
+    int count = 1; // change this
+
+
+    
+    *n = count; // <- set *n equal to number of corners in image.
+    descriptor *d = calloc(count, sizeof(descriptor));
+    //TODO: fill in array *d with descriptors of corners, use describe_index.
+
+
+    free_image(S);
+    free_image(R);
+    free_image(Rnms);
+    return d;
+}
---
+++ src/panorama_image.c
@@ -0,0 +1,374 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include "image.h"
+#include "matrix.h"
+
+// Comparator for matches
+// const void *a, *b: pointers to the matches to compare.
+// returns: result of comparison, 0 if same, 1 if a > b, -1 if a < b.
+int match_compare(const void *a, const void *b)
+{
+    match *ra = (match *)a;
+    match *rb = (match *)b;
+    if (ra->distance < rb->distance) return -1;
+    else if (ra->distance > rb->distance) return  1;
+    else return 0;
+}
+
+// Helper function to create 2d points.
+// float x, y: coordinates of point.
+// returns: the point.
+point make_point(float x, float y)
+{
+    point p;
+    p.x = x; p.y = y;
+    return p;
+}
+
+// Place two images side by side on canvas, for drawing matching pixels.
+// image a, b: images to place.
+// returns: image with both a and b side-by-side.
+image both_images(image a, image b)
+{
+    image both = make_image(a.w + b.w, a.h > b.h ? a.h : b.h, a.c > b.c ? a.c : b.c);
+    int i,j,k;
+    for(k = 0; k < a.c; ++k){
+        for(j = 0; j < a.h; ++j){
+            for(i = 0; i < a.w; ++i){
+                set_pixel(both, i, j, k, get_pixel(a, i, j, k));
+            }
+        }
+    }
+    for(k = 0; k < b.c; ++k){
+        for(j = 0; j < b.h; ++j){
+            for(i = 0; i < b.w; ++i){
+                set_pixel(both, i+a.w, j, k, get_pixel(b, i, j, k));
+            }
+        }
+    }
+    return both;
+}
+
+// Draws lines between matching pixels in two images.
+// image a, b: two images that have matches.
+// match *matches: array of matches between a and b.
+// int n: number of matches.
+// int inliers: number of inliers at beginning of matches, drawn in green.
+// returns: image with matches drawn between a and b on same canvas.
+image draw_matches(image a, image b, match *matches, int n, int inliers)
+{
+    image both = both_images(a, b);
+    int i,j;
+    for(i = 0; i < n; ++i){
+        int bx = matches[i].p.x; 
+        int ex = matches[i].q.x; 
+        int by = matches[i].p.y;
+        int ey = matches[i].q.y;
+        for(j = bx; j < ex + a.w; ++j){
+            int r = (float)(j-bx)/(ex+a.w - bx)*(ey - by) + by;
+            set_pixel(both, j, r, 0, i<inliers?0:1);
+            set_pixel(both, j, r, 1, i<inliers?1:0);
+            set_pixel(both, j, r, 2, 0);
+        }
+    }
+    return both;
+}
+
+// Draw the matches with inliers in green between two images.
+// image a, b: two images to match.
+// matches *
+image draw_inliers(image a, image b, matrix H, match *m, int n, float thresh)
+{
+    int inliers = model_inliers(H, m, n, thresh);
+    image lines = draw_matches(a, b, m, n, inliers);
+    return lines;
+}
+
+// Find corners, match them, and draw them between two images.
+// image a, b: images to match.
+// float sigma: gaussian for harris corner detector. Typical: 2
+// float thresh: threshold for corner/no corner. Typical: 1-5
+// int nms: window to perform nms on. Typical: 3
+image find_and_draw_matches(image a, image b, float sigma, float thresh, int nms)
+{
+    int an = 0;
+    int bn = 0;
+    int mn = 0;
+    descriptor *ad = harris_corner_detector(a, sigma, thresh, nms, &an);
+    descriptor *bd = harris_corner_detector(b, sigma, thresh, nms, &bn);
+    match *m = match_descriptors(ad, an, bd, bn, &mn);
+
+    mark_corners(a, ad, an);
+    mark_corners(b, bd, bn);
+    image lines = draw_matches(a, b, m, mn, 0);
+
+    free_descriptors(ad, an);
+    free_descriptors(bd, bn);
+    free(m);
+    return lines;
+}
+
+// Calculates L1 distance between to floating point arrays.
+// float *a, *b: arrays to compare.
+// int n: number of values in each array.
+// returns: l1 distance between arrays (sum of absolute differences).
+float l1_distance(float *a, float *b, int n)
+{
+    // TODO: return the correct number.
+    return 0;
+}
+
+// Finds best matches between descriptors of two images.
+// descriptor *a, *b: array of descriptors for pixels in two images.
+// int an, bn: number of descriptors in arrays a and b.
+// int *mn: pointer to number of matches found, to be filled in by function.
+// returns: best matches found. each descriptor in a should match with at most
+//          one other descriptor in b.
+match *match_descriptors(descriptor *a, int an, descriptor *b, int bn, int *mn)
+{
+    int i,j;
+
+    // We will have at most an matches.
+    *mn = an;
+    match *m = calloc(an, sizeof(match));
+    for(j = 0; j < an; ++j){
+        // TODO: for every descriptor in a, find best match in b.
+        // record ai as the index in *a and bi as the index in *b.
+        int bind = 0; // <- find the best match
+        m[j].ai = j;
+        m[j].bi = bind; // <- should be index in b.
+        m[j].p = a[j].p;
+        m[j].q = b[bind].p;
+        m[j].distance = 0; // <- should be the smallest L1 distance!
+    }
+
+    int count = 0;
+    int *seen = calloc(bn, sizeof(int));
+    // TODO: we want matches to be injective (one-to-one).
+    // Sort matches based on distance using match_compare and qsort.
+    // Then throw out matches to the same element in b. Use seen to keep track.
+    // Each point should only be a part of one match.
+    // Some points will not be in a match.
+    // In practice just bring good matches to front of list, set *mn.
+    *mn = count;
+    free(seen);
+    return m;
+}
+
+// Apply a projective transformation to a point.
+// matrix H: homography to project point.
+// point p: point to project.
+// returns: point projected using the homography.
+point project_point(matrix H, point p)
+{
+    matrix c = make_matrix(3, 1);
+    // TODO: project point p with homography H.
+    // Remember that homogeneous coordinates are equivalent up to scalar.
+    // Have to divide by.... something...
+    point q = make_point(0, 0);
+    return q;
+}
+
+// Calculate L2 distance between two points.
+// point p, q: points.
+// returns: L2 distance between them.
+float point_distance(point p, point q)
+{
+    // TODO: should be a quick one.
+}
+
+// Count number of inliers in a set of matches. Should also bring inliers
+// to the front of the array.
+// matrix H: homography between coordinate systems.
+// match *m: matches to compute inlier/outlier.
+// int n: number of matches in m.
+// float thresh: threshold to be an inlier.
+// returns: number of inliers whose projected point falls within thresh of
+//          their match in the other image. Should also rearrange matches
+//          so that the inliers are first in the array. For drawing.
+int model_inliers(matrix H, match *m, int n, float thresh)
+{
+    int i;
+    int count = 0;
+    // TODO: count number of matches that are inliers
+    // i.e. distance(H*p, q) < thresh
+    // Also, sort the matches m so the inliers are the first 'count' elements.
+    return count;
+}
+
+// Randomly shuffle matches for RANSAC.
+// match *m: matches to shuffle in place.
+// int n: number of elements in matches.
+void randomize_matches(match *m, int n)
+{
+    // TODO: implement Fisher-Yates to shuffle the array.
+}
+
+// Computes homography between two images given matching pixels.
+// match *matches: matching points between images.
+// int n: number of matches to use in calculating homography.
+// returns: matrix representing homography H that maps image a to image b.
+matrix compute_homography(match *matches, int n)
+{
+    matrix M = make_matrix(n*2, 8);
+    matrix b = make_matrix(n*2, 1);
+
+    int i;
+    for(i = 0; i < n; ++i){
+        double x  = matches[i].p.x;
+        double xp = matches[i].q.x;
+        double y  = matches[i].p.y;
+        double yp = matches[i].q.y;
+        // TODO: fill in the matrices M and b.
+
+    }
+    matrix a = solve_system(M, b);
+    free_matrix(M); free_matrix(b); 
+
+    // If a solution can't be found, return empty matrix;
+    matrix none = {0};
+    if(!a.data) return none;
+
+    matrix H = make_matrix(3, 3);
+    // TODO: fill in the homography H based on the result in a.
+
+
+    free_matrix(a);
+    return H;
+}
+
+// Perform RANdom SAmple Consensus to calculate homography for noisy matches.
+// match *m: set of matches.
+// int n: number of matches.
+// float thresh: inlier/outlier distance threshold.
+// int k: number of iterations to run.
+// int cutoff: inlier cutoff to exit early.
+// returns: matrix representing most common homography between matches.
+matrix RANSAC(match *m, int n, float thresh, int k, int cutoff)
+{
+    int e;
+    int best = 0;
+    matrix Hb = make_translation_homography(256, 0);
+    // TODO: fill in RANSAC algorithm.
+    // for k iterations:
+    //     shuffle the matches
+    //     compute a homography with a few matches (how many??)
+    //     if new homography is better than old (how can you tell?):
+    //         compute updated homography using all inliers
+    //         remember it and how good it is
+    //         if it's better than the cutoff:
+    //             return it immediately
+    // if we get to the end return the best homography
+    return Hb;
+}
+
+// Stitches two images together using a projective transformation.
+// image a, b: images to stitch.
+// matrix H: homography from image a coordinates to image b coordinates.
+// returns: combined image stitched together.
+image combine_images(image a, image b, matrix H)
+{
+    matrix Hinv = matrix_invert(H);
+
+    // Project the corners of image b into image a coordinates.
+    point c1 = project_point(Hinv, make_point(0,0));
+    point c2 = project_point(Hinv, make_point(b.w-1, 0));
+    point c3 = project_point(Hinv, make_point(0, b.h-1));
+    point c4 = project_point(Hinv, make_point(b.w-1, b.h-1));
+
+    // Find top left and bottom right corners of image b warped into image a.
+    point topleft, botright;
+    botright.x = MAX(c1.x, MAX(c2.x, MAX(c3.x, c4.x)));
+    botright.y = MAX(c1.y, MAX(c2.y, MAX(c3.y, c4.y)));
+    topleft.x = MIN(c1.x, MIN(c2.x, MIN(c3.x, c4.x)));
+    topleft.y = MIN(c1.y, MIN(c2.y, MIN(c3.y, c4.y)));
+
+    // Find how big our new image should be and the offsets from image a.
+    int dx = MIN(0, topleft.x);
+    int dy = MIN(0, topleft.y);
+    int w = MAX(a.w, botright.x) - dx;
+    int h = MAX(a.h, botright.y) - dy;
+
+    // Can disable this if you are making very big panoramas.
+    // Usually this means there was an error in calculating H.
+    if(w > 7000 || h > 7000){
+        fprintf(stderr, "output too big, stopping\n");
+        return copy_image(a);
+    }
+
+    int i,j,k;
+    image c = make_image(w, h, a.c);
+    
+    // Paste image a into the new image offset by dx and dy.
+    for(k = 0; k < a.c; ++k){
+        for(j = 0; j < a.h; ++j){
+            for(i = 0; i < a.w; ++i){
+                // TODO: fill in.
+            }
+        }
+    }
+
+    // TODO: Paste in image b as well.
+    // You should loop over some points in the new image (which? all?)
+    // and see if their projection from a coordinates to b coordinates falls
+    // inside of the bounds of image b. If so, use bilinear interpolation to
+    // estimate the value of b at that projection, then fill in image c.
+
+    return c;
+}
+
+// Create a panoramam between two images.
+// image a, b: images to stitch together.
+// float sigma: gaussian for harris corner detector. Typical: 2
+// float thresh: threshold for corner/no corner. Typical: 1-5
+// int nms: window to perform nms on. Typical: 3
+// float inlier_thresh: threshold for RANSAC inliers. Typical: 2-5
+// int iters: number of RANSAC iterations. Typical: 1,000-50,000
+// int cutoff: RANSAC inlier cutoff. Typical: 10-100
+image panorama_image(image a, image b, float sigma, float thresh, int nms, float inlier_thresh, int iters, int cutoff)
+{
+    srand(10);
+    int an = 0;
+    int bn = 0;
+    int mn = 0;
+    
+    // Calculate corners and descriptors
+    descriptor *ad = harris_corner_detector(a, sigma, thresh, nms, &an);
+    descriptor *bd = harris_corner_detector(b, sigma, thresh, nms, &bn);
+
+    // Find matches
+    match *m = match_descriptors(ad, an, bd, bn, &mn);
+
+    // Run RANSAC to find the homography
+    matrix H = RANSAC(m, mn, inlier_thresh, iters, cutoff);
+
+    if(1){
+        // Mark corners and matches between images
+        mark_corners(a, ad, an);
+        mark_corners(b, bd, bn);
+        image inlier_matches = draw_inliers(a, b, H, m, mn, inlier_thresh);
+        save_image(inlier_matches, "inliers");
+    }
+
+    free_descriptors(ad, an);
+    free_descriptors(bd, bn);
+    free(m);
+
+    // Stitch the images together with the homography
+    image comb = combine_images(a, b, H);
+    return comb;
+}
+
+// Project an image onto a cylinder.
+// image im: image to project.
+// float f: focal length used to take image (in pixels).
+// returns: image projected onto cylinder, then flattened.
+image cylindrical_project(image im, float f)
+{
+    //TODO: project image onto a cylinder
+    image c = copy_image(im);
+    return c;
+}
